fa4f89f4b513151ed9c05723e06d3e29edf81c00
Erik Schierboom <erik_schierboom@hotmail.com>
2026-02-17 13:49:49 +0100

    Rewrite stuff

--- Sleuth/Program.cs
+++ Sleuth/Program.cs
@@ -4,6 +4,6 @@ var codeCounts = await Codebase.CountLines("/Users/erik/Code/sleuth");
 foreach (var codeCount in codeCounts)
     Console.WriteLine($">{codeCount.File}: code: {codeCount.Code}, comments: {codeCount.Comments}, empty: {codeCount.Empty}, lines: {codeCount.Empty}");
 
-var frequentlyChangedFiles = VersionControl.MostFrequentlyChangedFiles("/Users/erik/Code/sleuth");
-foreach (var frequentlyChangedFile in frequentlyChangedFiles)
-    Console.WriteLine($">{frequentlyChangedFile.File}: {frequentlyChangedFile.Count}");
+var repositoryAnalysis = VersionControl.AnalyzeRepository("/Users/erik/Code/sleuth");
+foreach (var repositoryFileAnalysis in repositoryAnalysis.Files)
+    Console.WriteLine($">{repositoryFileAnalysis.File}: {repositoryFileAnalysis.NumberOfTimesChanged}");
--- Sleuth/VersionControl.cs
+++ Sleuth/VersionControl.cs
@@ -2,24 +2,39 @@ using LibGit2Sharp;
 
 namespace Sleuth;
 
-internal record FileChanges(string File, int Count);
+internal record VersionControlFileAnalysis(string File, int NumberOfTimesChanged, int NumberOfAuthors);
+
+internal record VersionControlRepositoryAnalysis(string Directory, VersionControlFileAnalysis[] Files);
 
 internal static class VersionControl
 {
-    public static FileChanges[] MostFrequentlyChangedFiles(string directoryPath)
+    public static VersionControlRepositoryAnalysis AnalyzeRepository(string directoryPath)
     {
         using var repo = new Repository(directoryPath);
 
-        return repo.Commits
-            .QueryBy(new CommitFilter { SortBy = CommitSortStrategies.Time | CommitSortStrategies.Reverse })
-            .TakeWhile(commit => commit.Committer.When >= DateTimeOffset.Now.AddYears(-1))
-            .SelectMany(commit =>
-                commit.Parents
-                    .SelectMany(parent => repo.Diff.Compare<TreeChanges>(parent.Tree, commit.Tree)
-                        .Select(change => change.OldPath)))
-            .GroupBy(path => path)
-            .Select(g => new FileChanges(g.Key, g.Count()))
-            .OrderByDescending(f => f.Count)
-            .ToArray();
+        var changesPerFile = new Dictionary<string, int>();
+        var authorsPerFile = new Dictionary<string, HashSet<string>>();
+
+        var compareOptions = new CompareOptions { IncludeUnmodified = false, Algorithm = DiffAlgorithm.Minimal };
+        foreach (var commit in repo.Commits)
+        {
+            var author = commit.Author.Name;
+
+            foreach (var parent in commit.Parents)
+            {
+                foreach (var change in repo.Diff.Compare<TreeChanges>(parent.Tree, commit.Tree, compareOptions))
+                {
+                    changesPerFile[change.Path] = changesPerFile.GetValueOrDefault(change.Path, 0) + 1;
+
+                    authorsPerFile.TryAdd(change.Path, []);
+                    authorsPerFile[change.Path].Add(author);
+                }
+            }
+        }
+        
+        HashSet<string> files = [..changesPerFile.Keys, ..authorsPerFile.Keys];
+        var fileAnalyses = files.Select(x => new VersionControlFileAnalysis(x, changesPerFile.GetValueOrDefault(x, 0), authorsPerFile.GetValueOrDefault(x, []).Count));
+
+        return new VersionControlRepositoryAnalysis(directoryPath, [..fileAnalyses]);
     }
 }
